5586ebeb99b85ae6c485cdea9727b60d89be6c4f
tokiniaina <hei.tokiniaina@gmail.com>
2025-05-15 23:56:16 +0300

    Invalid Choice

--- src/main/java/ussd/model/InvalidChoice.java
+++ src/main/java/ussd/model/InvalidChoice.java
@@ -1,2 +1,8 @@
-package ussd.model;public class InvalidChoice {
+package ussd.model;
+
+public class InvalidChoice {
+    public void invalidChoice (){
+        System.out.println("Choix invalide.");
+    }
+
 }
